fd291c3e62dd73f723e7d684ab5edac3eab24e83
doomcooin <samgravanorat@gmail.com>
2024-11-07 19:32:10 +0200

    m

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,47 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+// export default defineNuxtConfig({
+//   modules: [
+//     '@pinia/nuxt',
+//   ],
+//   pinia: {
+//     autoImports: ['defineStore'],
+//   },
+//   app: {
+//     head: {
+//       link: [
+//         {
+//           rel: 'stylesheet',
+//           href: 'https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.10.5/font/bootstrap-icons.min.css'
+//         },
+//         {
+
+//           rel: 'stylesheet',
+//           href: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css'
+//         },
+//       ],
+//       script: [
+//         {
+//           src: 'https://cdnjs.cloudflare.com/ajax/libs/bodymovin/5.7.6/lottie.min.js',
+//           defer: true
+//         }
+//       ]
+//     }
+//   },
+//   css: [
+//     'bootstrap/dist/css/bootstrap.min.css',
+//   ],
+//   compatibilityDate: '2024-04-03',
+//   devtools: { enabled: false},
+//   nitro: {
+//     watchOptions: {
+//       usePolling: true,
+//       interval: 1000,
+//       depth: 2
+//     },
+//   },
+// });
 export default defineNuxtConfig({
-  modules: [
-    '@pinia/nuxt',
-  ],
+  modules: ['@pinia/nuxt'],
   pinia: {
     autoImports: ['defineStore'],
   },
@@ -14,7 +53,6 @@ export default defineNuxtConfig({
           href: 'https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.10.5/font/bootstrap-icons.min.css'
         },
         {
-          
           rel: 'stylesheet',
           href: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css'
         },
@@ -31,12 +69,17 @@ export default defineNuxtConfig({
     'bootstrap/dist/css/bootstrap.min.css',
   ],
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: false},
+  devtools: { enabled: false },
   nitro: {
     watchOptions: {
       usePolling: true,
       interval: 1000,
-      depth: 2
+      depth: 2,
     },
   },
+  vite: {
+    optimizeDeps: {
+      include: ['axios']
+    }
+  }
 });
